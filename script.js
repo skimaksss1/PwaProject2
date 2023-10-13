49bce571031b0df909926050e0b5d460acb669e2
skimaksss1 <skimaksss@gmail.com>
2023-10-13 12:08:22 +0600

    commit

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ $(document).ready(function() {
 	}))
 });
 
+
+$(document).ready(function() {
+	addEventListener('load',()=>setTimeout(()=>{
+
+		$(".informAccordion").click(function(){
+			$(this).find(".disclosure").toggle();
+		
+
+
+			});
+			$(".informItem").click(function(){ 
+				$(this).find('svg').toggleClass('rotate'); 
+				$(this).find('.informName').toggleClass('greyColor'); 
+				$(this).find('.informAtr svg path').toggleClass('whiteColor'); 
+			});
+		
+	}))
+});
+
